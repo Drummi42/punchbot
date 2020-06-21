8c7e2a940bc936d2f73c54e0e1684cc576ef2553
Boris Menshakov <menshakovb@gmail.com>
2020-06-21 12:55:02 +0300

    fix: config

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/drummi42/punchbot/bot"
 	"github.com/drummi42/punchbot/config"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,7 +12,12 @@ import (
 
 func main() {
 	err := config.ReadConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
+	err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
